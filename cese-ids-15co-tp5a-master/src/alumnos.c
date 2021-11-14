fdfe95905a5bd7258c6c9d0cb02ed4cc17a09530
PJCarrizo <pablo.carrizoj@gmail.com>
2021-11-14 14:01:25 -0300

    Actualización con todos los prácticos

---
+++ cese-ids-15co-tp5a-master/src/alumnos.c
@@ -0,0 +1,59 @@
+/*=====[Module Name]===========================================================
+ * Copyright 2019 Esteban Daniel VOLENTINI <evolentini@gmail.com>
+ * All rights reserved.
+ * License: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>)
+ *
+ * Version: 0.1.0
+ * Creation Date: 2019/06/21
+ */
+ 
+/*=====[Inclusion of own header]=============================================*/
+
+#include "alumnos.h"
+#include <stdio.h>
+
+/*=====[Inclusions of private function dependencies]=========================*/
+
+/*=====[Definition macros of private constants]==============================*/
+
+/*=====[Private function-like macros]========================================*/
+
+/*=====[Definitions of private data types]===================================*/
+
+/*=====[Definitions of external public global variables]=====================*/
+
+/*=====[Definitions of public global variables]==============================*/
+
+/*=====[Definitions of private global variables]=============================*/
+
+/*=====[Prototypes (declarations) of private functions]======================*/
+
+/*=====[Implementations of public functions]=================================*/
+
+/*=====[Implementations of interrupt functions]==============================*/
+
+bool SerializarAlumno(char * cadena, size_t espacio, const alumno_t alumno) {
+    int resultado;
+    const char FORMATO[] = "{"
+        "\"documento\":\"%s\","
+        "\"apellidos\":\"%s\","
+        "\"nombres\":\"%s\""
+    "}";
+
+    resultado = snprintf(cadena, espacio, FORMATO, 
+             alumno->documento, alumno->apellidos, alumno->nombres);
+
+    return (resultado >= 0);
+}
+
+bool AlejandroPermingeat(char* cadena, size_t espacio) {
+    const struct alumno_s alumno = {
+        .apellidos = "PERMINGEAT",
+        .nombres = "Alejandro",
+        .documento = "99.999.999",
+    };
+
+    return SerializarAlumno(cadena, espacio, &alumno);
+}
+
+/*=====[Implementations of private functions]================================*/
